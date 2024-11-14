53d65e5fe6f7da3ff3cbe8ae64cc591d3b252f89
JoyceCristineDS <jcs3@cesar.school>
2024-11-14 16:09:58 -0300

    versão_final_2

--- crud/pet_shop/init.py
+++ crud/pet_shop/init.py
@@ -1,10 +1,8 @@
 from pet_shop.operations import venda_produtos, adicionar_produto, listar_produto, atualizar_produto, deletar_produto
-from pet_shop.operations import venda_produtos, adicionar_produto, listar_produto, atualizar_produto, deletar_produto
-print("===== PET SHOP - Cesar's Animal Hub =====")
+print("\n===== PET SHOP - Cesar's Animal Hub =====")
 
 def menu_geral():
-def menu_geral():
-    print("\nEscolha uma opção:")
+    print("\nEscolha uma opção:\n")
     print("1. Vendas")
     print("2. Cadastro de produtos")
     print("3. Voltar para página principal")
@@ -12,26 +10,26 @@ def menu_geral():
 def main():
     while True:
         menu_geral()
-        op_geral = input("Qual a opção desejada? ")
+        op_geral = input("\nQual a opção desejada? ")
         
         if op_geral == '1':
-            print(">>>> Produtos/Serviços disponíveis para venda <<<<")
+            print("\n>>>> Produtos/Serviços disponíveis para venda <<<<\n")
             listar_produto()
             venda_produtos()
-            print("Voltando para a página principal...")
+            print("\nVoltando para a página principal...\n")
             break
         
         elif op_geral == '2':
-            print("1. Adicionar produto")
+            print("\n1. Adicionar produto")
             print("2. Listar produto")
             print("3. Atualizar produto")
             print("4. Deletar produto")
             print("5. Voltar a página inicial")
 
-            op_cadastro = int(input("Qual a opção desejada? "))
+            op_cadastro = input("\nQual a opção desejada? ")
 
             if op_cadastro == '1':
-                nome = input("Digite o nome do produto: ")
+                nome = input("\nDigite o nome do produto: ")
                 tipo = input("Digite o tipo do produto: ")
                 preco = input("Digite o preço do produto: ")
                 adicionar_produto(nome, tipo, preco)
@@ -40,27 +38,29 @@ def main():
                 listar_produto()
             
             elif op_cadastro == '3':
-                id = int(input("Digite o ID do produto a ser atualizado: "))
-                novo_nome = input("Digite o novo nome do produto: ")
+                id = int(input("\nDigite o ID do produto a ser atualizado: "))
+                novo_nome = input("\nDigite o novo nome do produto: ")
                 novo_tipo = input("Digite a novo tipo do produto: ")
                 novo_preco = input("Digite a novo preço do produto: ")
                 atualizar_produto(id, novo_nome, novo_tipo, novo_preco)
             
             elif op_cadastro == '4':
-                id = int(input("Digite o ID do produto a ser deletado: "))
+                id = int(input("\nDigite o ID do produto a ser deletado: "))
                 deletar_produto(id)
 
             elif op_cadastro == '5':
-                print("Voltando ao menu principal...")
+                print("\nVoltando ao menu principal...")
                 break
 
             else:
-                print("Opção inválida! Tente novamente.")
+                print("\nOpção inválida! Tente novamente.")
+        elif op_geral == 3:
+            print("\nVoltando ao menu principal...")
+            break
+        else:
+            print("\nOpção inválida! Tente novamente.")
+            break
 
-
-
-            else:
-                print("Opção inválida! Tente novamente.")
 
 
 
--- crud/pet_shop/operations.py
+++ crud/pet_shop/operations.py
@@ -22,13 +22,6 @@ def gerar_id_unico(dados):
         maior_id = max(produto["id"] for produto in dados)
         return maior_id + 1
 
-def gerar_id_unico(dados):
-    if not dados:
-        return 1  
-    else:
-        maior_id = max(produto["id"] for produto in dados)
-        return maior_id + 1
-
 def adicionar_produto(nome, tipo, preco):
     dados = carregar_dados()
     novo_id = gerar_id_unico(dados) 
@@ -36,7 +29,7 @@ def adicionar_produto(nome, tipo, preco):
         "id": novo_id,
         "nome": nome,
         "tipo": tipo,
-        "preço": preco
+        "preco": preco
     }
     dados.append(novo_produto)
     salvar_dados(dados)
@@ -56,7 +49,7 @@ def atualizar_produto(id, novo_nome, novo_tipo, novo_preco):
         if produto["id"] == id:
             produto["nome"] = novo_nome
             produto["tipo"] = novo_tipo
-            produto["preço"] = novo_preco 
+            produto["preco"] = novo_preco 
             salvar_dados(dados)
             print("Produto atualizado com sucesso!")
             return
@@ -71,205 +64,75 @@ def deletar_produto(id):
 def venda_produtos():
     acumulador_1 = 0
     while True:
-        op_produto = input("Escolha o produto desejado (ou digite 'sair' para finalizar): ")
+        op_produto = input("\nEscolha o produto desejado (ou digite 'sair' para finalizar): ")
         if op_produto.lower() == 'sair':
             print(f"\nO valor final da sua compra é: R$ {acumulador_1:.2f}")
-            print("Agradecemos a preferência, até a próxima!")
+            print("\nAgradecemos a preferência, até a próxima!")
             break
         
         match op_produto:
             case '1': 
                 preco_unitario = 50
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '2': 
                 preco_unitario = 20
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '3': 
                 preco_unitario = 35
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '4': 
                 preco_unitario = 15
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '5': 
                 preco_unitario = 80
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '6': 
                 preco_unitario = 50
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '7': 
                 preco_unitario = 100
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '8': 
                 preco_unitario = 70
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '9': 
                 preco_unitario = 150
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '10':
                 preco_unitario = 80
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '11': 
                 preco_unitario = 150
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '12':
                 preco_unitario = 200
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '13': 
                 preco_unitario = 250
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '14': 
                 preco_unitario = 500
-                quant = int(input("Qual a quantidade que você deseja? "))
+                quant = int(input("\nQual a quantidade que você deseja? "))
                 valor = preco_unitario * quant
             case '15': 
                 preco_unitario = 100
-                meses = int(input("Deseja contratar quantos meses de serviço? "))
+                meses = int(input("\nDeseja contratar quantos meses de serviço? "))
                 quant = meses
                 valor = preco_unitario * quant
         
         acumulador_1 += valor
-        print(f"Produto adicionado! Valor total acumulado da compra até agora: R$ {acumulador_1:.2f}\n")
-
-
-
-import json
-import os
-import pet_shop.init as pet_shop
-
-DATA_FILE_PATH = os.path.join(os.path.dirname(__file__), "data.json")
-
-def carregar_dados():
-    try:
-        with open(DATA_FILE_PATH, "r", encoding="utf-8") as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
-
-def salvar_dados(dados):
-    with open(DATA_FILE_PATH, "w", encoding="utf-8") as file:
-        json.dump(dados, file, indent=4)
-
-def gerar_id_unico(dados):
-    if not dados:
-        return 1  
-    else:
-        maior_id = max(produto["id"] for produto in dados)
-        return maior_id + 1
-
-def gerar_id_unico(dados):
-    if not dados:
-        return 1  
-    else:
-        maior_id = max(produto["id"] for produto in dados)
-        return maior_id + 1
-
-def adicionar_produto(nome, tipo, preco):
-    dados = carregar_dados()
-    novo_id = gerar_id_unico(dados) 
-    novo_produto = {
-        "id": novo_id,
-        "nome": nome,
-        "tipo": tipo,
-        "preço": preco
-    }
-    dados.append(novo_produto)
-    salvar_dados(dados)
-    print(f"Produto cadastrado com sucesso! ID: {novo_id}")
-
-def listar_produto():
-    dados = carregar_dados()
-    if not dados: 
-        print("Nenhum produto encontrado.")
-        return
-    for produto in dados:
-        print(f"ID: {produto['id']} - Nome: {produto['nome']}, - Tipo: {produto['tipo']}, - Preço: {produto['preco']}")
-
-def atualizar_produto(id, novo_nome, novo_tipo, novo_preco):
-    dados = carregar_dados()
-    for produto in dados:
-        if produto["id"] == id:
-            produto["nome"] = novo_nome
-            produto["tipo"] = novo_tipo
-            produto["preço"] = novo_preco 
-            salvar_dados(dados)
-            print("Produto atualizado com sucesso!")
-            return
-    print("Produto não encontrado.")
-
-def deletar_produto(id):
-    dados = carregar_dados()
-    dados = [produto for produto in dados if produto["id"] != id]
-    salvar_dados(dados)
-    print("Produto removido com sucesso!")
-
-def venda_produtos():
-        acumulador_1 = 0
-        while True:
-            op_produto = input("Escolha o produto desejado: ")
-            match op_produto:
-                case '1': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 50 * quant
-                case '2': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 20 * quant
-                case '3': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 35 * quant
-                case '4': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 15 * quant
-                case '5': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 80 * quant
-                case '6': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 50 * quant
-                case '7': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 100 * quant
-                case '8': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 70 * quant
-                case '9': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 150 * quant
-                case '10':
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 80 * quant
-                case '11': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 150 * quant
-                case '12':
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 200 * quant
-                case '13': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 250 * quant
-                case '14': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 500 * quant
-                case '15': 
-                        meses = int(input("Deseja contratar quantos meses de serviço? "))
-                        valor = 100 * meses
-            acumulador_1 = acumulador_1 + valor
-            print("O valor acumulado dos produtos é: R$", acumulador_1)
-            resposta_1 = input("\nDeseja continuar comprando? (S/N) ")
-            if resposta_1.lower() == 's':
-                pet_shop.main()
-            else:
-                print("Agradecemos a preferência, até a próxima!")
+        print(f"\nProduto adicionado! Valor total acumulado da compra até agora: R$ {acumulador_1:.2f}\n")
 
