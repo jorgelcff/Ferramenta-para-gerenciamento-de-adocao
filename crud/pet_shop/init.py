074dc74c7216c62e6f2ad26b010ea70ab0c66c95
albeansantiago <ascb@cesar.school>
2024-11-13 22:45:59 -0300

    versão compilada
    modificação na lista

--- crud/pet_shop/init.py
+++ crud/pet_shop/init.py
@@ -1,4 +1,4 @@
-from pet_shop.operations import venda_produtos, venda_servicos, venda_pacotes, venda_planos, adicionar_produto, listar_produto, atualizar_produto, deletar_produto
+from pet_shop.operations import venda_produtos, adicionar_produto, listar_produto, atualizar_produto, deletar_produto
 print("===== PET SHOP - Cesar's Animal Hub =====")
 
 def menu_geral():
@@ -7,63 +7,15 @@ def menu_geral():
     print("2. Cadastro de produtos")
     print("3. Voltar para página principal")
 
-def menu_vendas():
-    print("1. Produtos")
-    print("2. Serviços")
-    print("3. Pacotes Especiais")
-    print("4. Planos de Saúde")
-    print("5. Voltar para página inicial")
-
 def main():
     while True:
         menu_geral()
         op_geral = input("Qual a opção desejada? ")
         
         if op_geral == '1':
-            menu_vendas()
-            op_vendas = input("Escolha a opção: ")
-
-            if op_vendas == '1':
-                print(">>>> Produtos disponíveis <<<<")
-                
-                listar_produto()
-
-                venda_produtos()
-
-            elif op_vendas == '2':
-                print(">>>> Serviços disponíveis <<<<")
-                print("1. Banho e tosa - R$ 50,00")
-                print("2. Consulta veterinária - R$ 100,00")
-                print("3. Vacinação - R$ 70,00")
-                print("4. Adestramento - R$ 150,00")
-                print("5. Hotel para pets - R$ 80,00/noite")
-
-                venda_servicos()
-
-            elif op_vendas == '3':
-                print(">>>> Nossos Pacotes Especiais <<<<")
-                print("1. Pacote de Banho Mensal - R$ 150,00")
-                print("2. Pacote de Tosa Completa - R$ 200,00")
-                print("3. Pacote Saúde Completo (Consulta + Vacinação) - R$ 250,00")
-                print("4. Pacote Adestramento - R$ 500,00")
-
-                venda_pacotes()
-
-            elif op_vendas == '4':
-                print(">>>> Nossos Planos de Saúde <<<<")
-                print("1. Plano Básico - R$ 100,00/mês")
-                print("2. Plano Intermediário - R$ 150,00/mês")
-                print("3. Plano Avançado - R$ 200,00/mês")
-                print("4. Plano Premium - R$ 300,00/mês")
-
-                venda_planos()
-
-            elif op_vendas == '5':
-                print("Voltando ao Menu Principal...")
-                break  
-
-            else:
-                print("Opção inválida. Por favor, escolha uma opção válida.")
+            print(">>>> Produtos/Serviços disponíveis para venda <<<<")
+            listar_produto()
+            venda_produtos()
         
         elif op_geral == '2':
             print("1. Adicionar produto")
--- crud/pet_shop/operations.py
+++ crud/pet_shop/operations.py
@@ -88,6 +88,36 @@ def venda_produtos():
                 case '5': 
                         quant = int(input("Qual a quantidade que você deseja? "))
                         valor = 80 * quant
+                case '6': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 50 * quant
+                case '7': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 100 * quant
+                case '8': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 70 * quant
+                case '9': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 150 * quant
+                case '10':
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 80 * quant
+                case '11': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 150 * quant
+                case '12':
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 200 * quant
+                case '13': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 250 * quant
+                case '14': 
+                        quant = int(input("Qual a quantidade que você deseja? "))
+                        valor = 500 * quant
+                case '15': 
+                        meses = int(input("Deseja contratar quantos meses de serviço? "))
+                        valor = 100 * meses
             acumulador_1 = acumulador_1 + valor
             print("O valor acumulado dos produtos é: R$", acumulador_1)
             resposta_1 = input("\nDeseja continuar comprando? (S/N) ")
@@ -96,81 +126,3 @@ def venda_produtos():
             else:
                 print("Agradecemos a preferência, até a próxima!")
 
-def venda_servicos():
-        acumulador_2 = 0
-        while True: 
-            op_servico = input("Escolha o serviço desejado: ")
-            match op_servico:
-                case '1': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 50 * quant
-                case '2': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 100 * quant
-                case '3': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 70 * quant
-                case '4': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 150 * quant
-                case '5':
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 80 * quant
-            acumulador_2 = acumulador_2 + valor
-            print("O valor acumulado dos serviços é: R$", acumulador_2)
-            resposta_2 = input("\nDeseja continuar comprando? (S/N) ")
-            if resposta_2.lower() == 'S':
-                pet_shop.main()
-                break
-            elif resposta_2.lower() == 'N':
-                print("Agradecemos a preferência, até a próxima!")
-                break
-
-def venda_pacotes():
-        acumulador_3 = 0
-        while True:
-            op_pacote = input("Escolha o pacote desejado: ")
-            match op_pacote:
-                case '1': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 150 * quant
-                case '2':
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 200 * quant
-                case '3': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 250 * quant
-                case '4': 
-                        quant = int(input("Qual a quantidade que você deseja? "))
-                        valor = 500 * quant
-            acumulador_3 = acumulador_3 + valor
-            print("O valor acumulado dos pacotes especiais é: R$", acumulador_3)
-            resposta_3 = input("\nDeseja continuar comprando? (S/N) ")
-            if resposta_3.lower() == 'S':
-                  pet_shop.main()
-            else:
-                print("Agradecemos a preferência, até a próxima!")
-
-def venda_planos():
-            acumulador_4 = 0
-            op_plano = input("Escolha um plano de saúde adequado para o seu Pet: ")
-            match op_plano:
-                case '1': 
-                        meses = int(input("Deseja contratar quantos meses de serviço? "))
-                        valor = 100 * meses
-                case '2': 
-                        meses = int(input("Deseja contratar quantos meses de serviço? "))
-                        valor = 150 * meses
-                case '3': 
-                        meses = int(input("Deseja contratar quantos meses de serviço? "))
-                        valor = 200 * meses
-                case '4': 
-                        meses = int(input("Deseja contratar quantos meses de serviço? "))
-                        valor = 300 * meses
-            acumulador_4 = acumulador_4 + valor
-            print("O valor acumulado dos planos de saúde é: R$", acumulador_4)
-            resposta_4 = input("\nDeseja continuar comprando? (S/N) ")
-            if resposta_4.lower() == 'S':
-                pet_shop.main()
-            else:
-                print("Agradecemos a preferência, até a próxima!")
